40b0ac668cfb5309cb3d8da765bb7b7b78a5cf41
rico becker <rico.becker@kf-interactive.com>
2013-08-10 22:01:16 +0200

    adds dummy implementation

--- KFOpenWeatherMapAPI/KFViewController.m
+++ KFOpenWeatherMapAPI/KFViewController.m
@@ -7,23 +7,44 @@
 //
 
 #import "KFViewController.h"
+#import "KFOpenWeatherMapAPIClient.h"
 
 @interface KFViewController ()
 
+
+@property (nonatomic, strong) KFOpenWeatherMapAPIClient *apiClient;
+
+
 @end
 
+
 @implementation KFViewController
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.apiClient = [[KFOpenWeatherMapAPIClient alloc] initWithAPIKey:@"1111111111" andAPIVersion:@"2.5"];
+    [self.apiClient weatherForCityName:@"Los Angeles" withResultBlock:^(BOOL success, id responseData, NSError *error)
+    {
+        if (success)
+        {
+            NSLog(@"received weather: %@", responseData);
+        }
+        else
+        {
+            NSLog(@"could not get weather");
+        }
+        
+    }];
+    
 }
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
 
 @end
