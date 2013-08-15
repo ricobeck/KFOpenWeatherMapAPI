72db5a072c4d3e06698a9f74e22faf8ac51a2af7
rico becker <rico.becker@kf-interactive.com>
2013-08-15 21:34:38 +0200

    adds osx target

---
+++ OSXExample/KFOSXAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KFAppDelegate.m
+//  OSXExample
+//
+//  Created by rick on 15/08/13.
+//  Copyright (c) 2013 KF Interactive. All rights reserved.
+//
+
+#import "KFOSXAppDelegate.h"
+
+@implementation KFOSXAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
